39ae6af4d3de78859b85a6842c4a1fb889fcf155
Subin Lee <subin1719@gmail.com>
2023-08-16 23:58:45 -0400

    gpt message and sendmessagebar components

--- app/components/SendMessageBar.tsx
+++ app/components/SendMessageBar.tsx
@@ -2,16 +2,27 @@ import React from "react";
 
 const SendMessageBar = () => {
     return (
-        <div>
-            <div className="flex flex-col w-full">
-                <input
-                    className=""
-                    type="text"
-                    name="message"
-                    id="message"
-                    placeholder="Try asking a question!"
-                />
-            </div>
+        <div className="flex w-1/2 items-center my-4 mt-16 rounded-lg border">
+            <input
+                type="text"
+                placeholder="Type a question!"
+                className="flex-1 p-3 rounded-l-lg focus:outline-none"
+            />
+            <button className="p-3 text-slate-600 rounded-r-lg focus:outline-none">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6">
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+                    />
+                </svg>
+            </button>
         </div>
     );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import SendMessageBar from "@/app/components/SendMessageBar";
+import GPTMessage from "./components/GPTMessage";
 
 export default function Page() {
     return (
         <main className="flex h-screen flex-col items-center p-16">
-            <div className="w-full h-3/4 mx-auto bg-white rounded-lg shadow-md">
+            <div className="w-full h-3/4 bg-white rounded-lg shadow-md">
                 <div className="text-white bg-cyan-800 h-auto p-10 flex">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -21,45 +22,9 @@ export default function Page() {
                     </svg>
                     User message goes here.
                 </div>
-                <div className="bg-gray-300 text-gray-700 h-auto p-10 flex">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-6 h-6 mr-3">
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
-                        />
-                    </svg>
-                    System response goes here.
-                </div>
+                <GPTMessage message="System response goes here" />
             </div>
-            <div className="flex w-1/2 items-center my-4 mt-16">
-                <input
-                    type="text"
-                    placeholder="Type a question!"
-                    className="flex-1 p-2 rounded-l-lg border focus:outline-none"
-                />
-                <button className="bg-cyan-800 text-white p-2 rounded-r-lg hover:bg-blue-600 focus:outline-none">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-6 h-6">
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-                        />
-                    </svg>
-                </button>
-            </div>
+            <SendMessageBar />
         </main>
     );
 }
