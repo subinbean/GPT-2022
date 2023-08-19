9f166d063895e7abdea10ef9595b1153e3dadcb5
Subin Lee <subin1719@gmail.com>
2023-08-18 23:17:33 -0400

    backend route for generate response created

--- api/index.py
+++ api/index.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from generate_message import generate_message
 
 app = Flask(__name__)
 
 
+@app.route("/api/generate-response", methods=["POST"])
+def generate_response():
+    data = request.json
+    print(data)
+    query = data['query']
+    print(query)
+    system_message = generate_message(query)
+    response = {"data": system_message}
+    return response, 200
+
+
 @app.route("/api/healthcheck", methods=["GET"])
-def healthCheck():
+def health_check():
     return {'status': 'success', 'message': 'Flask integrated with Next!'}
