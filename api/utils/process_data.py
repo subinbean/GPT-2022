22d18db96374f78a5ee0a6f4620c079912004192
Subin Lee <subin1719@gmail.com>
2023-09-02 23:47:48 -0400

    added regex support for parsing title of pdf

--- api/utils/process_data.py
+++ api/utils/process_data.py
@@ -6,12 +6,16 @@ import os
 from dotenv import load_dotenv
 import pinecone
 import pinecone_setup
+import re
 
 load_dotenv()
 
 try:
     # load the pdf
-    loader = PyPDFLoader('../../assets/Apple-10k-2022.pdf')
+    pdf_path = '../../assets/Apple-10k-2022.pdf'
+    loader = PyPDFLoader(pdf_path)
+    title = re.findall(r'/([^/]+)\.pdf$', pdf_path)[0]
+    print(title)
     raw_file = loader.load()
 
     # split up the raw text of pdf
@@ -31,7 +35,7 @@ try:
     # initialize embeddings model
     embeddings = OpenAIEmbeddings()
 
-    # # pinecone
+    # pinecone
     # pinecone_setup.pinecone_setup()
     # index_name = os.environ.get("PINECONE_INDEX_NAME")
     # index = pinecone.Index(index_name)
