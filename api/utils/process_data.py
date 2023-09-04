cc6e1ab6afdf5137b891ec9ff7bbb7857d590b85
Subin Lee <subin1719@gmail.com>
2023-09-03 23:53:12 -0400

    changes to process_data script to process multiple pdfs

--- api/utils/process_data.py
+++ api/utils/process_data.py
@@ -10,38 +10,50 @@ import re
 
 load_dotenv()
 
-try:
-    # load the pdf
-    pdf_path = '../../assets/Apple-10k-2022.pdf'
-    loader = PyPDFLoader(pdf_path)
-    title = re.findall(r'/([^/]+)\.pdf$', pdf_path)[0]
-    print(title)
-    raw_file = loader.load()
 
-    # split up the raw text of pdf
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=200
-    )
-    documents = text_splitter.split_documents(raw_file)
-    print('docs loaded and split')
+def ingest_file(pdf_path):
+    try:
+        # extract title of pdf
+        title = re.findall(r'/([^/]+)\.pdf$', pdf_path)[0]
+        print(title)
+        loader = PyPDFLoader(pdf_path)
+        raw_file = loader.load()
 
-    # sanity check
-    with open('sample.txt', 'w') as file:
-        for d in documents:
-            file.write(str(d))
-            file.write('\n --section--\n')
+        # split up the raw text of pdf
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200
+        )
+        documents = text_splitter.split_documents(raw_file)
+        print('docs loaded and split')
 
-    # initialize embeddings model
-    embeddings = OpenAIEmbeddings()
+        # sanity check
+        with open('sample.txt', 'w') as file:
+            for d in documents:
+                file.write(str(d))
+                file.write('\n --section--\n')
 
-    # pinecone
-    # pinecone_setup.pinecone_setup()
-    # index_name = os.environ.get("PINECONE_INDEX_NAME")
-    # index = pinecone.Index(index_name)
-    # print('creating embeddings and storing in pinecone')
-    # Pinecone.from_documents(documents, embeddings, index_name=index_name)
-    # print('Done!')
+        # initialize embeddings model
+        embeddings = OpenAIEmbeddings()
 
-except Exception as e:
-    print('An error occurred:', e)
+        # pinecone
+        pinecone_setup.pinecone_setup()
+        index_name = os.environ.get("PINECONE_INDEX_NAME")
+        index = pinecone.Index(index_name)
+        print('creating embeddings and storing in pinecone')
+        Pinecone.from_documents(documents, embeddings, index_name=index_name)
+        print('Done!')
+
+    except Exception as e:
+        print('An error occurred:', e)
+
+
+def main():
+    folder_path = 'assets'
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            ingest_file(os.path.join(root, file))
+
+
+if __name__ == '__main__':
+    main()
