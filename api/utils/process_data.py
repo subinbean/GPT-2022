5085e9bd5e9b6bc03a23e5e46ee49aa1b4e5c3f7
Subin Lee <subin1719@gmail.com>
2023-09-20 17:32:09 -0400

    modify upserting script a bit

--- api/utils/process_data.py
+++ api/utils/process_data.py
@@ -4,7 +4,6 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
 import os
 from dotenv import load_dotenv
-import pinecone
 import pinecone_setup
 import re
 
@@ -28,22 +27,21 @@ def ingest_file(pdf_path):
         print('docs loaded and split')
 
         # sanity check
-        with open(f'{title}.txt', 'w') as file:
-            for d in documents:
-                file.write(str(d))
-                file.write('\n --section--\n')
+        # with open(f'{title}.txt', 'w') as file:
+        #     for d in documents:
+        #         file.write(str(d))
+        #         file.write('\n --section--\n')
 
         # initialize embeddings model
         embeddings = OpenAIEmbeddings()
 
         # pinecone
-        # pinecone_setup.pinecone_setup()
-        # index_name = os.environ.get("PINECONE_INDEX_NAME")
-        # index = pinecone.Index(index_name)
-        # print('creating embeddings and storing in pinecone')
-        # Pinecone.from_documents(documents, embeddings,
-        #                         index_name=index_name, namespace=title)
-        # print('Done!')
+        pinecone_setup.pinecone_setup()
+        index_name = os.environ.get("PINECONE_INDEX_NAME")
+        print('creating embeddings and storing in pinecone')
+        Pinecone.from_documents(documents, embeddings,
+                                index_name=index_name, namespace=title)
+        print('Done!')
 
     except Exception as e:
         print('An error occurred:', e)
